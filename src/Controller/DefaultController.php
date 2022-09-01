7afb5f029e8289091d5aea0a9c127c63623290ff
Didix6 <didi-twix@live.fr>
2022-09-01 15:39:56 +0200

    create entity

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,6 @@ class DefaultController extends AbstractController
     #[Route('/home/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('program/index.html.twig');
+        return $this->render('/base.html.twig');
     }
 }
